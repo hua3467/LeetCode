d66a42014d277aaec2bb900acfeb461a6b9b30cf
AaronYang <zhenhuayang@outlook.com>
2018-12-18 21:35:56 -0600

    Initial commit

---
+++ src/easy/ReverseInteger.java
@@ -0,0 +1,34 @@
+package easy;
+import java.util.LinkedList;
+/**
+ *
+ * @author aaronyang
+ */
+public class ReverseInteger {
+    public static void main(String[] args ){
+        
+        System.out.println(reverse(153423646));
+    }
+    
+    public static int reverse(int x){
+        
+        LinkedList<Integer> stack = new LinkedList<>();
+        
+        int count = 0;
+        
+        while ( Math.abs(x) > 0 ){
+            stack.push( x % 10 );
+            x = x / 10;
+            count++;
+        }
+        
+        for(int i = 0; i < count; i++){
+            x += stack.pop() * Math.pow(10, i);
+        }
+        
+        if( x >= Integer.MAX_VALUE || x <= Integer.MIN_VALUE )
+            return 0;
+        else 
+            return x;
+    }
+}
